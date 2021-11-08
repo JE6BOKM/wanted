62ff36f614bcf129661b3453af6126ce69a09cc4
김민규 <2000y03m13d@gimmingyus-MacBook-Air.local>
2021-11-08 22:24:57 +0900

    STYLE: edit conventions

--- apps/company_info/models.py
+++ apps/company_info/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Language(models.Model):
     name = models.CharField(max_length=2)
 
@@ -9,15 +10,19 @@ class Language(models.Model):
     def __str__(self):
         return self.name
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
-    language = models.ForeignKey("Language", on_delete=models.CASCADE, related_name="tags")
+    language = models.ForeignKey(
+        "Language", on_delete=models.CASCADE, related_name="tags"
+    )
 
     class Meta:
         db_table = "tags"
 
     def __str__(self):
         return self.name
+
 
 class Company(models.Model):
     name = models.CharField(max_length=50)
