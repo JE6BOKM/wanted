4af735623fb8440f26ba7a323bca7bcdbbdb620f
Sung Muk Kang <miranaky@gmail.com>
2021-11-09 22:28:50 +0900

    Update serializers.py

--- apps/company_info/serializers.py
+++ apps/company_info/serializers.py
@@ -9,8 +9,8 @@ class CompanyListSerializer(ModelSerializer):
     class Meta:
         model = CompanyName
         fields = ("company_name",)
-        
-        
+
+
 class CompanyDetailSerializer(ModelSerializer):
     company_name = CharField(source="name")
     tags = StringRelatedField(many=True, source="c_id.tags")
