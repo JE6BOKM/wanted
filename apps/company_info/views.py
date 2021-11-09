0f0d787675409a83369a81ddbc24f537bde07f7a
sungmuk Kang <miranaky@gmail.com>
2021-11-09 19:08:23 +0900

    FIX: Not Found 항목 추가
    - 이름으로 검색 시 없을 경우 Not Found 가 나타나도록 변경

--- apps/company_info/views.py
+++ apps/company_info/views.py
@@ -7,12 +7,21 @@ from apps.company_info.serializers import CompanyListSerializer
 
 
 class CompanySearchView(APIView):
+    """Company search view with query"""
+
     def get(self, request):
         name = request.GET.get("query", None)
         language = request.META.get("HTTP_X_WANTED_LANGUAGE")
 
         company_list = CompanyName.objects.filter(
-            language__name=language, name__contains=name
+            language__name=language, name__icontains=name
         )
+        print(company_list)
+        print(len(company_list))
+        if len(company_list) == 0:
+            return Response(
+                {"error": f"There is no company with that name like {name}.  "},
+                status=status.HTTP_404_NOT_FOUND,
+            )
         serializer = CompanyListSerializer(company_list, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
